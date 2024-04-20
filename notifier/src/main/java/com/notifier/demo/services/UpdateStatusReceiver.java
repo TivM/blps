417459e5539bc5dcbc98459a797f2ac285330c9f
vladislavtrofimcenko <337009@niuitmo.ru>
2024-04-20 13:42:03 +0300

    add atomikos in notifier

--- notifier/src/main/java/com/notifier/demo/services/UpdateStatusReceiver.java
+++ notifier/src/main/java/com/notifier/demo/services/UpdateStatusReceiver.java
@@ -3,8 +3,8 @@ package com.notifier.demo.services;
 import com.notifier.demo.dto.ItemForKafka;
 import com.notifier.demo.entity.Notification;
 import com.notifier.demo.repository.NotificationRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +14,7 @@ public class UpdateStatusReceiver {
 
     private final NotificationRepository notificationRepository;
 
+    @Transactional
     @KafkaListener(topics = "fuck-you", groupId = "user-group", containerFactory = "factory")
     public void listener(ItemForKafka item){
         notificationRepository.save(Notification.of(item));
